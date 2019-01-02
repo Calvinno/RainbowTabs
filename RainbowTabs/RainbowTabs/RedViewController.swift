f23fbcb965f049f1d6bd618f827f414934a3db74
Calvinno <42626008+Calvinno@users.noreply.github.com>
2019-01-02 15:35:58 -0500

    Add files via upload

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Calvin Cantin on 18-09-14.
+//  Copyright © 2018 Calvin Cantin. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "!"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
